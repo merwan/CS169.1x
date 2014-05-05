a0cadf3f4049904e8b060b6b4858d7490bbcd5dd
Merouane Atig <merouane.atig@gmail.com>
2014-05-05 22:00:59 +0000

    first implementation of starts_with_consonant?

--- hw0-2.rb
+++ hw0-2.rb
@@ -1,3 +1,8 @@
 def hello(name)
   "Hello, #{name}"
 end
+
+VOYELS = ["A", "E", "I", "O", "U"]
+def starts_with_consonant?(text)
+  !VOYELS.include?(text[0].upcase)
+end
--- hw0-2_spec.rb
+++ hw0-2_spec.rb
@@ -5,3 +5,17 @@ describe "#hello" do
     hello("bob").should eq("Hello, bob")
   end
 end
+
+describe "#starts_with_consonant?" do
+  it "returns false when starts with a voyel" do
+    starts_with_consonant?("aze").should be_false
+    starts_with_consonant?("ert").should be_false
+    starts_with_consonant?("iop").should be_false
+    starts_with_consonant?("op^l").should be_false
+    starts_with_consonant?("uio").should be_false
+  end
+
+  it "returns true when starts with a consonant" do
+    starts_with_consonant?("qsd").should be_true
+  end
+end
