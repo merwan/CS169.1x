0f577c82251d33f4b04080f70cbb0c8552ba6977
Merouane Atig <merouane.atig@gmail.com>
2014-05-13 21:46:00 +0200

    implement game and validation

--- HW1/lib/rock_paper_scissors.rb
+++ HW1/lib/rock_paper_scissors.rb
@@ -4,7 +4,22 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    s1 = player1[1]
+    s2 = player2[1]
+    validate_strategy s1
+    validate_strategy s2
+    if s1 == 'R'
+      return player2 if s2 == 'P'
+    elsif s1 == 'P'
+      return player2 if s2 == 'S'
+    else
+      return player2 if s2 == 'R'
+    end
+    player1
+  end
+
+  def self.validate_strategy(strategy)
+    raise NoSuchStrategyError.new('Strategy must be one of R,P,S') unless strategy.match /[PRS]/
   end
 
   def self.tournament_winner(tournament)
