d3a25fb2746df3ea8613821877d84e6156eedd33
Merouane Atig <merouane.atig@gmail.com>
2014-05-13 20:58:23 +0200

    implement anagram

--- HW1/lib/fun_with_strings.rb
+++ HW1/lib/fun_with_strings.rb
@@ -5,14 +5,32 @@ module FunWithStrings
   end
   def count_words
     words = {}
-    self.downcase.scan(/\w+/).each do |word|
+    get_words.each do |word|
       words[word] = 0 unless words.has_key? word
       words[word] += 1
     end
     words
   end
   def anagram_groups
-    []
+    anagrams = []
+    get_words.each do |source|
+      group = []
+      get_words.each do |w|
+        next if w.length != source.length
+        is_anagram = true
+        source.each_char do |c|
+          is_anagram = false unless w.include? c
+        end
+        group << w if is_anagram
+      end
+      anagrams << group if group.length > 0
+    end
+    anagrams
+  end
+
+  private
+  def get_words
+    self.downcase.scan(/\w+/)
   end
 end
 
